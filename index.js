63c5166e035987805fdf4ff0dec74bf9e4b055fc
shairyleannramos <iamjustineramos99@gmail.com>
2024-05-25 18:48:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function clickLike1(){
 let totalLikes = parseInt(countLike1.value) + 1
   countLike.textContent = totalLikes.toString()
 }
-btbLike1.addEventListener("click",clickLike1)
+btnLike1.addEventListener("click",clickLike1)
 
